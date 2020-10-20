6cab25c003de019e8fd18399d7ac6eca15d971fc
Dhanush <dhanush1926@gmail.com>
2020-10-20 20:17:41 +0530

    Updates

--- precache-manifest.f2a0fa8aaf1122798c20302f37f639f8.js
+++ precache-manifest.f2a0fa8aaf1122798c20302f37f639f8.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b183a5c330db302b9b673bcfab3e809",
+    "revision": "740e431e01b82db866d930804d84f71e",
     "url": "/code-book/index.html"
   },
   {
-    "revision": "5a8b5e02ba9695f5ec35",
-    "url": "/code-book/static/css/main.c493d3e5.chunk.css"
+    "revision": "05c6ea9fceab38700fbf",
+    "url": "/code-book/static/css/main.a79fbbc0.chunk.css"
   },
   {
-    "revision": "6689fd50e516fc95521f",
-    "url": "/code-book/static/js/2.ba173064.chunk.js"
+    "revision": "b18558e0efd014ee3f23",
+    "url": "/code-book/static/js/2.5696b572.chunk.js"
   },
   {
     "revision": "971748a654c86e1387914d4e8287e22f",
-    "url": "/code-book/static/js/2.ba173064.chunk.js.LICENSE.txt"
+    "url": "/code-book/static/js/2.5696b572.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a8b5e02ba9695f5ec35",
-    "url": "/code-book/static/js/main.c8809839.chunk.js"
+    "revision": "05c6ea9fceab38700fbf",
+    "url": "/code-book/static/js/main.46572e6b.chunk.js"
   },
   {
     "revision": "82f1d4669f796282e329",
     "url": "/code-book/static/js/runtime-main.fef7f046.js"
+  },
+  {
+    "revision": "09c87fcc5903202e8e7a34b3ee8a3482",
+    "url": "/code-book/static/media/find-the-duplicate-number.09c87fcc.png"
   },
   {
     "revision": "0424a39dfd55a37b7cf41ce11aef4d1c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/code-book/precache-manifest.5182d53038f358b2329db78fd9c393b3.js"
+  "/code-book/precache-manifest.f2a0fa8aaf1122798c20302f37f639f8.js"
 );
 
 self.addEventListener('message', (event) => {
